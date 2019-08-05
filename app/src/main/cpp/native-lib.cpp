f7108220777fd04cc636e71eaf57864c16d21ec2
sufeng <sufeng@missfresh.cn>
2019-08-05 19:50:01 +0800

    init

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,45 @@
+#include <jni.h>
+#include <string>
+
+std::string ab = "";
+
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_testndk_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = ab;
+    return env->NewStringUTF(hello.c_str());
+}
+
+std::string jstring2str(JNIEnv* env, jstring jstr)
+{
+    char*   rtn   =   NULL;
+    jclass   clsstring   =   env->FindClass("java/lang/String");
+    jstring   strencode   =   env->NewStringUTF("GB2312");
+    jmethodID   mid   =   env->GetMethodID(clsstring,   "getBytes",   "(Ljava/lang/String;)[B");
+    jbyteArray   barr=   (jbyteArray)env->CallObjectMethod(jstr,mid,strencode);
+    jsize   alen   =   env->GetArrayLength(barr);
+    jbyte*   ba   =   env->GetByteArrayElements(barr,JNI_FALSE);
+    if(alen   >   0)
+    {
+        rtn   =   (char*)malloc(alen+1);
+        memcpy(rtn,ba,alen);
+        rtn[alen]=0;
+    }
+    env->ReleaseByteArrayElements(barr,ba,0);
+    std::string stemp(rtn);
+    free(rtn);
+    return   stemp;
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_testndk_Utils_write(JNIEnv *env, jclass type, jstring password_) {
+    const char *password = env->GetStringUTFChars(password_, 0);
+
+    ab = jstring2str(env,password_);
+    // TODO
+    env->ReleaseStringUTFChars(password_, password);
+}
+
---
+++ app/src/main/java/com/example/testndk/Utils.java
@@ -0,0 +1,13 @@
+package com.example.testndk;
+
+/**
+ * File description.
+ *
+ * @author sufeng
+ * @date 2019-08-05
+ */
+public class Utils {
+
+    public static native void write(String password);
+
+}
